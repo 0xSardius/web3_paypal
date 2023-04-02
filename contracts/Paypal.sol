0594c303182123fff4fc3cbc3ca645a2a72d4c78
Sardius <0xsardius@gmail.com>
2023-04-02 11:03:07 -0400

    Initial commit

---
+++ contracts/Paypal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract PayPal {
+    // Define the owner of the contract
+    // Create the Struct and Mapping for request, transaction, and name
+    // Add a name to wallet address
+    // Create a request
+    // Pay a request
+    // Get all the requests sent to the user
+    // Get all historic transactions user has been a part of
+}
